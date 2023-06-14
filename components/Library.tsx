a6d01ad0eb383724a3654f24e0a6a691927dc047
marijn111 <39378193+marijn111@users.noreply.github.com>
2023-06-14 21:54:27 +0200

    added song upload, media library and actions

--- components/Library.tsx
+++ components/Library.tsx
@@ -3,9 +3,30 @@
 import { TbPlaylist } from "react-icons/tb";
 import { AiOutlinePlus } from "react-icons/ai";
 
-const Library = () => {
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
+import useUploadModal from "@/hooks/useUploadModal";
+import { Song } from "@/types";
+
+import MediaItem from "./MediaItem";
+
+interface LibraryProps {
+    songs: Song[];
+}
+
+const Library: React.FC<LibraryProps> = ({
+    songs
+}) => {
+    const authModal = useAuthModal();
+    const uploadModal = useUploadModal();
+    const { user } = useUser();
+
     const onClick = () => {
-        // handle upload later
+        if (!user) {
+            return authModal.onOpen();
+        }
+        // TODO: check for subscription
+        return uploadModal.onOpen();
     };
 
     return (
@@ -20,7 +41,9 @@ const Library = () => {
                 <AiOutlinePlus onClick={onClick} size={20} className="text-neutral-400 cursor-pointer hover:text-white transition"/>
             </div>
             <div className="flex flex-col gap-y-2 mt-4 px-3">
-                List of Songs!
+                {songs.map((item) => (
+                    <MediaItem onClick={() => {}} key={item.id} data={item} />
+                ))}
             </div>
         </div>
     );
